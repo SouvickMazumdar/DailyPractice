d73e982eaee02d3cf3ec0e9114084f14ae144329
SouvickMazumdar <69057478+SouvickMazumdar@users.noreply.github.com>
2024-12-30 18:25:42 +0530

    second commit

---
+++ GFG/POTD/2024/December/Q30.py
@@ -0,0 +1,6 @@
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def findUnion(self, a, b):
+        # code here
+       return len(set(a).union(set(b)))
